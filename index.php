b87604991f8c2d0bf85b12be5cf3336becf28d58
unknown <vieiracf1@LC112PC10.active.newpaltz.edu>
2014-08-28 10:37:23 -0400

    First commit in aptana

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>index</title>
+		
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>index</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by vieiracf1
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
