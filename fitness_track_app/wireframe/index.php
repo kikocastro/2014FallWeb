2809c091ff9f5dbff777d3c2936bc489bed1c0eb
Kiko Castro <castro.112358@gmail.com>
2014-09-08 15:35:21 +0000

    index page

---
+++ fitness_track_app/wireframe/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Home</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+		<link href="css/index.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<div class="container">
+				<h1>Welcome to SUNY Fitness Track App!</h1>
+				</div>
+			</header>
+
+    <div class="container">
+    
+    	<div class="row">
+			  <div class="col-md-6">
+			  	<form class="form-sign" role="form">
+	      	<h2 class="form-sign-heading">Sign Up</h2>
+	      	<input type="name" class="form-control" placeholder="Name" required autofocus>
+	        <input type="email" class="form-control" placeholder="Email address" required autofocus>
+	        &nbsp;
+	        <button class="btn btn-lg btn-primary btn-block" type="submit">Let's go!</button>
+	        </form>
+			  
+			  </div>
+			  <div class="col-md-6">
+			  	<form class="form-sign" role="form">
+	      	<h2 class="form-sign-heading">Good to see you again</h2>
+	        <input type="email" class="form-control" placeholder="Email address" required autofocus>
+	        <input type="password" class="form-control" placeholder="Password" required>
+	        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	        </form>
+			  </div>
+			</div>
+
+
+      
+
+    </div> <!-- /container -->
+
+
+			<footer>
+				<div class="container">
+					<p>
+						&copy; Copyright
+					</p>
+				</div>
+			</footer>
+		</div>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</body>
+</html>
