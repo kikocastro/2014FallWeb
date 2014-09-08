273b5a01b009b7158711c28f6d0551465c88516a
unknown <vieiracf1@LC112PC18.active.newpaltz.edu>
2014-09-08 10:48:19 -0400

    bootstrap

--- playground/index.php
+++ playground/index.php
@@ -2,33 +2,45 @@
 <html lang="en">
 	<head>
 		<title>index</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 		
 	</head>
 
 	<body>
 		<div>
 			<header>
+				<div class="container">
 				<h1>this was edited in Aptana</h1>
 				<h2>Today is Sep 4th</h2>
+				</div>
 			</header>
 			<nav>
-				<p>
-					<a href="/">Home</a>
-				</p>
-				<p>
-					<a href="/contact">Contact</a>
-				</p>
+				<div class="container">
+					<p>
+						<a href="/">Home</a>
+					</p>
+					<p>
+						<a href="/contact">Contact</a>
+					</p>
+				</div>
 			</nav>
 
 			<div>
+				<div class="container">
+				</div>
 
 			</div>
 
 			<footer>
-				<p>
-					&copy; Copyright  by vieiracf1
-				</p>
+				<div class="container">
+					<p>
+						&copy; Copyright  by vieiracf1
+					</p>
+				</div>
 			</footer>
 		</div>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
