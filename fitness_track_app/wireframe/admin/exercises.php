f0be2ca62ff5373141585fc0a15cff748cd78855
Frederico Castro <castro.112358@gmail.com>
2014-09-09 23:28:46 -0400

    fixed table

--- fitness_track_app/wireframe/admin/exercises.php
+++ fitness_track_app/wireframe/admin/exercises.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>SUNY Fitness Tracking</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-		<link href="../css/admin/admin.css" rel="stylesheet">
-	</head>
+<head>
+  <title>SUNY Fitness Tracking</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  <link href="../css/admin/admin.css" rel="stylesheet">
+</head>
 
-	<body>
-		<div>
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<body>
+  <div>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container-fluid">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+            <!-- <span class="icon-bar"></span> -->
           </button>
           <a class="navbar-brand" href="#">Suny Fitness Track App</a>
         </div>
@@ -38,64 +38,65 @@
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
             <li><a href="#">Overview</a></li>
-            <li class="active"><a href="#">Exercise Categories</a></li>
-            <li><a href="#">Exercises</a></li>
+            <li><a href="exercises-categories.php">Exercise Categories</a></li>
+            <li class="active"><a href="exercises.php">Exercises</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Exercises Categories</h1>
-
+          <h1 class="page-header">Exercises List</h1>
           <div class="row placeholders">
-            <div class="row">
-			  <div class="col-lg-6 col-lg-offset-3">
-			    <div class="input-group top-bottom-margin">
-			      <input type="text" class="form-control">
-			      <span class="input-group-btn">
-			        <button class="btn btn-default" type="button">Add</button>
-			      </span>
-			    </div><!-- /input-group -->
-			  </div><!-- /.col-lg-6 -->
+            <div class="col-lg-6 col-lg-offset-3">
+              <div class="input-group top-bottom-margin">
+                <input type="text" class="form-control">
+                <span class="input-group-btn">
+                  <button class="btn btn-default" type="button">Add</button>
+                </span>
+              </div><!-- /input-group -->
+            </div><!-- /.col-lg-6 -->
           </div>
-			
-			
-			
+
           <div class="table-responsive">
             <table class="table table-striped">
               <thead>
                 <tr>
                   <th>Name</th>
-                  <th>Description</th>
+                  <th>Category</th>
+                  <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>Endurance</td>
                   <td>Lorem Ipsum</td>
+                  <td>view edit delete</td>
                 </tr>
                 <tr>
                   <td>Strength</td>
                   <td>amet consectetur</td>
+                  <td>view edit delete</td>
                 </tr>
                 <tr>
                   <td>Balance</td>
                   <td>amet consectetur</td>
+                  <td>view edit delete</td>
                 </tr>
+
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    </div>  
+    </div>
 
 
-  		<footer>
-  			<div class="container">
-  				<p>
-  					&copy; Copyright
-  				</p>
-  			</div>
-  		</footer>
-		</div>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	</body>
+    <footer>
+     <div class="container">
+      <p>
+       &copy; Copyright
+     </p>
+   </div>
+ </footer>
+</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
 </html>
