4a27d13ce9a3d1f699c356f92cd70741244d605f
Frederico Castro <castro.112358@gmail.com>
2014-09-10 15:17:58 -0400

    changed layout

--- fitness_track_app/wireframe/exercises-lists/new.php
+++ fitness_track_app/wireframe/exercises-lists/new.php
@@ -37,128 +37,125 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="dashboard.php">Dashboard</a></li>
+            <li class="active"><a href="../dashboard.php">Dashboard</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">New Exercise List</h1>
           &nbsp;
+          <div>
+            <button type="submit" class="btn btn-success pull-right">Add Exercise</button>
+          </div>
           <div class="row">
-            <div class="bs-example">
-              <form class="form-horizontal">
-                <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="category" class="control-label col-xs-3">Category</label>
-                    <div class="col-xs-8">
-                      <select class="form-control">
-                        <option>Endurance</option>
-                        <option>Strength</option>
-                        <option>Balance</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="name" class="control-label col-xs-3">Name</label>
-                    <div class="col-xs-8">
-                      <select class="form-control">
-                        <option>Ex1</option>
-                        <option>Ex2</option>
-                        <option>Ex3</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="start-date" class="control-label col-xs-3">Start Date</label>
-                    <div class="col-xs-8">
-                      <input type="date" class="form-control" id="start-date" placeholder="Insert start date">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="end-date" class="control-label col-xs-3">End Date</label>
-                    <div class="col-xs-8">
-                      <input type="date" class="form-control" id="end-date" placeholder="Insert end date">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="description" class="control-label col-xs-3">Description</label>
-                    <div class="col-xs-8">
-                      <textarea class="form-control" rows="3"></textarea>
-                    </div>
-                  </div>
-                  &nbsp;
-                  <div class="form-group">
-                    <div class="col-xs-offset-6 col-xs-8">
-                      <button type="submit" class="btn btn-primary">Save changes</button>
-                      <button type="button" class="btn">Cancel</button>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-
-
-                  <div class="table-responsive">
-                   <table class="table table-striped">
-                    <thead>
-                      <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Endurance</td>
-                        <td>Lorem Ipsum</td>
-                        <td>
-                          <button type="submit" class="btn btn-primary">
-                            <i class="glyphicon glyphicon-pencil icon-white"></i> 
-                          </button>
-                          <button type="submit" class="btn btn-primary">
-                            <i class="glyphicon glyphicon-remove icon-white"></i> 
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Strength</td>
-                        <td>amet consectetur</td>
-                        <td>
-                          <button type="submit" class="btn btn-primary">
-                            <i class="glyphicon glyphicon-pencil icon-white"></i> 
-                          </button>
-                          <button type="submit" class="btn btn-primary">
-                            <i class="glyphicon glyphicon-remove icon-white"></i> 
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td>Balance</td>
-                        <td>amet consectetur</td>
-                        <td>
-                          <button type="submit" class="btn btn-primary">
-                            <i class="glyphicon glyphicon-pencil icon-white"></i> 
-                          </button>
-                          <button type="submit" class="btn btn-primary">
-                            <i class="glyphicon glyphicon-remove icon-white"></i> 
-                          </button>
-                        </td>
-                      </tr>
-                    </table>
-                  </div>
-                  &nbsp;
-                  <div class="col-sm-4 col-sm-offset-8">
-                    <button type="submit" class="btn btn-success">Add Exercise</button>
-                  </div>
-                </div>
-
-              </form>
+            <div class="table-responsive">
+             <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Endurance</td>
+                  <td>Lorem Ipsum</td>
+                  <td>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="glyphicon glyphicon-pencil icon-white"></i> 
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="glyphicon glyphicon-remove icon-white"></i> 
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Strength</td>
+                  <td>amet consectetur</td>
+                  <td>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="glyphicon glyphicon-pencil icon-white"></i> 
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="glyphicon glyphicon-remove icon-white"></i> 
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Balance</td>
+                  <td>amet consectetur</td>
+                  <td>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="glyphicon glyphicon-pencil icon-white"></i> 
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="glyphicon glyphicon-remove icon-white"></i> 
+                    </button>
+                  </td>
+                </tr>
+              </table>
             </div>
-
+            &nbsp;
 
           </div>
+          &nbsp;
+
+          <div class="row">
+            <form class="form-horizontal">
+              <div class="col-sm-6 col-sm-offset-3">
+                <div class="form-group">
+                  <label for="category" class="control-label col-xs-3">Category</label>
+                  <div class="col-xs-8">
+                    <select class="form-control">
+                      <option>Endurance</option>
+                      <option>Strength</option>
+                      <option>Balance</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="name" class="control-label col-xs-3">Name</label>
+                  <div class="col-xs-8">
+                    <select class="form-control">
+                      <option>Ex1</option>
+                      <option>Ex2</option>
+                      <option>Ex3</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="start-date" class="control-label col-xs-3">Start Date</label>
+                  <div class="col-xs-8">
+                    <input type="date" class="form-control" id="start-date" placeholder="Insert start date">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="end-date" class="control-label col-xs-3">End Date</label>
+                  <div class="col-xs-8">
+                    <input type="date" class="form-control" id="end-date" placeholder="Insert end date">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="description" class="control-label col-xs-3">Description</label>
+                  <div class="col-xs-8">
+                    <textarea class="form-control" rows="3"></textarea>
+                  </div>
+                </div>
+                &nbsp;
+                <div class="form-group">
+                  <div class="col-xs-offset-6 col-xs-8">
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn">Cancel</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+
         </div>
       </div>
 
