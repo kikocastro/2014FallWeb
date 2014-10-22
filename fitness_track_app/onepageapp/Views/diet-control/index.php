7a2dbf29dc84e032cb2991360b88c7a6d40c6387
Frederico Castro <castro.112358@gmail.com>
2014-10-22 01:18:49 -0400

    diet control mvc - not complete

---
+++ fitness_track_app/onepageapp/Views/diet-control/index.php
@@ -0,0 +1,9 @@
+<div class="row">
+	<div class="col-lg-12 text-center">
+		<h2 class="section-heading">Diet Control</h2>
+		<h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+	</div>
+</div>
+<div class="row">
+
+</div>
---
+++ fitness_track_app/onepageapp/Views/shared/_template.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>SUNY Fitness Tracking APP</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="../../content/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="../../content/css/agency.css" rel="stylesheet">
+
+		<!-- Custom Fonts -->
+		<link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+		<link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body id="page-top" class="index">
+
+		<!-- Navigation -->
+		<nav class="navbar navbar-default navbar-fixed-top navbar-shrink"">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header page-scroll">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand page-scroll" href="#page-top">SUNY Fitness Tracking APP</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="hidden">
+							<a href="#page-top"></a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#exercises-lists">Exercises Lists</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#fitness-reports">Fitness Reports</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="diet-control">Diet Control</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#diet-report">Diet Report</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#contact">Profile</a>
+						</li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+
+		<? include __DIR__ . '/../' . $view; ?>
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<span class="copyright">Copyright &copy; SUNY Fitness Tracking App 2014</span>
+					</div>
+					<div class="col-md-4">
+						<ul class="list-inline social-buttons">
+							<li>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-facebook"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-4">
+						<ul class="list-inline quicklinks">
+							<li>
+								<a href="#">Privacy Policy</a>
+							</li>
+							<li>
+								<a href="#">Terms of Use</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<!-- jQuery Version 1.11.0 -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+		<!-- Bootstrap Core JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+		<!-- Plugin JavaScript -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+		<script src="../../js/classie.js"></script>
+		<script src="../../js/cbpAnimatedHeader.js"></script>
+
+		<!-- Contact Form JavaScript -->
+		<script src="../../js/jqBootstrapValidation.js"></script>
+		<script src="../../js/contact_me.js"></script>
+
+		<!-- Custom Theme JavaScript -->
+		<script src="../../js/agency.js"></script>
+
+		<!-- high charts -->
+		<script src="http://code.highcharts.com/highcharts.js"></script>
+		<script type="text/javascript" src="js/high_chart_test.js"></script>
+
+	</body>
+
+</html>
