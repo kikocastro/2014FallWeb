b75525420f47f4e64a091ee644eb50dfd1284e0c
Frederico Castro <castro.112358@gmail.com>
2014-10-23 00:30:06 -0400

    modal with edit

--- fitness_track_app/onepageapp/Views/diet-control/edit.php
+++ fitness_track_app/onepageapp/Views/diet-control/edit.php
@@ -1,1 +1,40 @@
-edit
+<div class="row">
+	<form class="form-horizontal">
+		<div class="col-sm-8 col-sm-offset-2">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Insert the name *" id="name" required="" data-validation-required-message="Please enter the name.">
+				<p class="help-block text-danger"></p>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Insert the calories *" id="cal" required="" data-validation-required-message="Please enter the calories.">
+				<p class="help-block text-danger"></p>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Insert the fat *" id="fat" required="" data-validation-required-message="Please enter the fat.">
+				<p class="help-block text-danger"></p>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Insert the carbs *" id="carbs" required="" data-validation-required-message="Please enter the carbs.">
+				<p class="help-block text-danger"></p>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Insert the protein *" id="protein" required="" data-validation-required-message="Please enter the protein.">
+				<p class="help-block text-danger"></p>
+			</div>
+			<div class="form-group">
+				<label for="date" class="control-label col-xs-4">Date and time</label>
+				<div class="col-xs-8">
+					<input type="datetime-local" class="form-control" id="txtTime" placeholder="Time">
+				</div>
+			</div>
+			
+
+			<div class="col-lg-12 text-right">
+				<div id="success"></div>
+				<button type="submit" class="btn btn-xl">
+					Save
+				</button>
+			</div>
+		</div>
+	</form>
+</div>
--- fitness_track_app/onepageapp/Views/diet-control/index.php
+++ fitness_track_app/onepageapp/Views/diet-control/index.php
@@ -3,50 +3,60 @@
 		<h2 class="section-heading">Diet Control</h2>
 	</div>
 </div>
+<div class="pull-right">
+	<a data-toggle="modal" data-target="#newListModal" class="page-scroll btn btn-xl"  href="?action=edit&format=plain">+</a>
+</div>
 <!-- table of food -->
 <div class="row ">
-  <div class="col-lg-12">
-    <div class="table-responsive">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Calories</th>
-            <th>Fat</th>
-            <th>Carbs</th>
-            <th>Protein</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td><span class="label label-default">200</span></td>
-            <td>12</td>
-            <td>80</td>
-            <td>6</td>
-            <td>Monday 7:45</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td><span class="label label-default">200</span></td>
-            <td>12</td>
-            <td>80</td>
-            <td>6</td>
-            <td>Monday 7:45</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td><span class="label label-default">200</span></td>
-            <td>12</td>
-            <td>80</td>
-            <td>6</td>
-            <td>Monday 7:45</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+	<div class="col-lg-12">
+		<div class="table-responsive">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Calories</th>
+						<th>Fat</th>
+						<th>Carbs</th>
+						<th>Protein</th>
+						<th>Time</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td><span class="label label-default">200</span></td>
+						<td>12</td>
+						<td>80</td>
+						<td>6</td>
+						<td>Monday 7:45</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td><span class="label label-default">200</span></td>
+						<td>12</td>
+						<td>80</td>
+						<td>6</td>
+						<td>Monday 7:45</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td><span class="label label-default">200</span></td>
+						<td>12</td>
+						<td>80</td>
+						<td>6</td>
+						<td>Monday 7:45</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 
+<!-- modal -->
+<div class="modal fade" id="newListModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		</div>
+	</div>
+</div>
 
