8ad3a6d3bd73a558811a67cb67ecfc51207be2c9
Frederico Castro <castro.112358@gmail.com>
2014-11-24 21:17:18 -0500

    food types added

--- fitness_track_app/onepageapp/includes/_all.php
+++ fitness_track_app/onepageapp/includes/_all.php
@@ -1,5 +1,6 @@
 <?php
 	include_once __DIR__ . '/_global.php';
 	include_once __DIR__ . '/../Models/food.php';
-	include_once __DIR__ . '/../Models/exercise.php';
-	include_once __DIR__ . '/../Models/user.php';
+	include_once __DIR__ . '/../Models/food-types.php';
+  include_once __DIR__ . '/../Models/exercise.php';
+  include_once __DIR__ . '/../Models/user.php';
