93d5823ba7a93eed6ef21a1fa820290e63bc888e
Frederico Castro <castro.112358@gmail.com>
2014-12-03 22:08:29 -0500

    fix food types

--- fitness_track_app/onepageapp/Controllers/food-types.php
+++ fitness_track_app/onepageapp/Controllers/food-types.php
@@ -8,71 +8,69 @@ $view   = null;
 
 switch ($action . '_' . $method) {
 	case 'create_GET':
-	$model = FoodType::Blank();
-	$view = "food-types/edit.php";
-	break;
+		$model = FoodType::Blank();
+		$view = "food-types/edit.php";
+		break;
 	case 'save_POST':
-	
-	$sub_action = empty($_REQUEST['id']) ? 'created' : 'updated';
-	$errors = FoodType::Validate($_REQUEST);
-	if(!$errors){
-		$errors = FoodType::Save($_REQUEST);
-	}
+		$sub_action = empty($_REQUEST['id']) ? 'created' : 'updated';
+		$errors = FoodType::Validate($_REQUEST);
+		if(!$errors){
+			$errors = FoodType::Save($_REQUEST);
+		}
 
-	if(!$errors){
-		if($format == 'json'){
-			header("Location: ?action=edit&format=json&id=$_REQUEST[id]");
+		if(!$errors){
+			if($format == 'json'){
+				header("Location: ?action=edit&format=json&id=$_REQUEST[id]");
+			}else{
+				header("Location: ?sub_action=$sub_action&id=$_REQUEST[id]");
+			}
+			die();
+		}else{
+			$model = array( $_REQUEST, $errors);
+			$view = "food-types/edit.php";    
+		}
+		break;
+	case 'delete':
+		if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	        //Promt
+		}else{
+
+		}
+		break;
+	case 'edit_GET':
+		$model = FoodType::Get($_REQUEST['id']);
+		$view = "food-types/edit.php";    
+		break;
+	case 'delete_GET':
+		$model = FoodType::Get($_REQUEST['id']);
+		$view = "food-types/delete.php";    
+		break;
+	case 'delete_POST':
+		$errors = FoodType::Delete($_REQUEST['id']);
+		if($errors){
+			$model = FoodType::Get($_REQUEST['id']);
+			$view = "food-types/delete.php";
 		}else{
 			header("Location: ?sub_action=$sub_action&id=$_REQUEST[id]");
+			die();      
 		}
-		die();
-	}else{
-		$model = $_REQUEST;
-		$view = "food-types/edit.php";    
-	}
-	break;
-	case 'delete':
-	if($_SERVER['REQUEST_METHOD'] == 'GET'){
-        //Promt
-	}else{
-
-	}
-	break;
-	break;
-	case 'edit_GET':
-	$model = FoodType::Get($_REQUEST['id']);
-	$view = "food-types/edit.php";    
-	break;
-	case 'delete_GET':
-	$model = FoodType::Get($_REQUEST['id']);
-	$view = "food-types/delete.php";    
-	break;
-	case 'delete_POST':
-	$errors = FoodType::Delete($_REQUEST['id']);
-	if($errors){
-		$model = FoodType::Get($_REQUEST['id']);
-		$view = "food-types/delete.php";
-	}else{
-		header("Location: ?sub_action=$sub_action&id=$_REQUEST[id]");
-		die();      
-	}
-	break;
+		break;
 	case 'index_GET':
-	default:
-	$model = FoodType::Get();
-	$view = 'food-types/index.php';   
-	break;
+		default:
+		$model = FoodType::Get();
+		$view = 'food-types/index.php';   
+		break;
 }
 
 switch ($format) {
 	case 'json':
-	echo json_encode($model);
-	break;
+		echo json_encode($model);
+		break;
 	case 'plain':
-	include __DIR__ . "/../Views/$view";    
-	break;    
+		include __DIR__ . "/../Views/$view";    
+		break;    
 	case 'web':
-	default:
-	include __DIR__ . "/../Views/shared/_template.php";   
-	break;
+		default:
+		include __DIR__ . "/../Views/shared/_template.php";   
+		break;
 }
